39b52214865248b738d8f35dc60015277b0db8ea
JWWilson <jwwilson@anydayemployment.com>
2018-11-02 18:00:32 -0700

    Navigation devleopment

--- client-side/src/components/navbar.js
+++ client-side/src/components/navbar.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import Venderlist from './venderlist';
-
+import {
+  Route,
+  Redirect,
+  NavLink,
+  Link,
+  browserHistory
+} from 'react-router-dom';
+import Links from './links';
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-        
+      toVendorList: false
     };
   }
 
   searchDates(e) {
     console.log('Search Dates');
   }
-  searchLocations(e) {
+  searchLocations() {
     console.log('Search Locations');
   }
 
-  searchCategories=()=> {
-    <Venderlist/>
+  searchCategories= ()=> {
+    //browserHistory.push('/venderlist');
+    //this.setState({ toVendorList: true });
+    //<Link to='/venderlist'></Link>
+    //<Route path='/venderlist' component={Venderlist}  />
+    //<Redirect to='/venderlist' />;
     console.log('Search Categories');
   }
 
   render() {
+    if (this.state.toVendorList === true) {
+      console.log('toVenderlist = True');
+      <Redirect to='/venderlist' />;
+    }
+
     return (
       <div className="container">
         <div id="myNav">
-          <nav >
+          <nav>
             <form className="form-inline">
               <button
                 className="btn btn-outline-primary nav-button"
@@ -45,7 +61,7 @@ class Navbar extends React.Component {
               </button>
               <button
                 className="btn btn-outline-primary nav-button"
-                onClick={() => this.searchCategories()}
+                onClick={this.searchCategories}
                 type="button"
               >
                 Categories
